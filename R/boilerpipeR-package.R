c95197e289a93cd59c5388e5a7e5104e66132be8
mannau <mario.annau@gmail.com>
2014-05-12 08:23:23 +0200

    Remove path in data help page

--- R/boilerpipeR-package.R
+++ R/boilerpipeR-package.R
@@ -32,7 +32,7 @@ NULL
 #' url <- "http://quantivity.wordpress.com/2012/11/09/multi-asset-market-regimes/"
 #' content <- getURL(url)
 #' content <- iconv(content, "UTF-8", "ASCII//TRANSLIT")
-#' save(content, file = "/home/mario/Dropbox/Private/workspace/sentiment/pkg/boilerpipeR/data/content.rda")
+#' save(content, file = "content.rda")
 #' }
 NULL
 
